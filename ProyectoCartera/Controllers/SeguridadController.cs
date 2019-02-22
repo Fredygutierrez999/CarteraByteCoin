3b485baf8283581bfdc390cc630db5b2e3fb1c82
Fredy Gutierrez <fredy.gutierrez999@gmail.com>
2019-02-21 23:08:23 -0500

    FG: Creación de proyecto CarteraByteCoin.

---
+++ ProyectoCartera/Controllers/SeguridadController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using System.Web.Http;
+using ProyectoCartera.App_Start;
+using ProyectoCartera.Models.ModeloClases.Seguridad;
+
+namespace ProyectoCartera.Controllers
+{
+    /// <summary>
+    /// Controlador utilizado para administrar 
+    /// </summary>
+    [AllowAnonymous]
+    [RoutePrefix("api/Seguridad")]
+    public class SeguridadController : ApiController
+    {
+        [HttpPost]
+        [Route("authenticate")]
+        public IHttpActionResult Authenticate(Usuarios login)
+        {
+            if (login == null)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            //TODO: Validate credentials Correctly, this code is only for demo !!
+            bool isCredentialValid = (login.Clave == "123456");
+            if (isCredentialValid)
+            {
+                var token = TokenValidationHandler.GenerateTokenJwt(login.Usuario);
+                return Ok(token);
+            }
+            else
+            {
+                return Unauthorized();
+            }
+        }
+
+    }
+}
